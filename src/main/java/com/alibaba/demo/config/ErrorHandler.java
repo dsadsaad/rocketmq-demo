d3a1faf86d88e5ef5d4b505b1de3069ad1b8068e
Jarvis <dsadsaad@gmail.com>
2020-07-23 11:36:19 +0800

    更新group方式订阅

--- src/main/java/com/alibaba/demo/config/ErrorHandler.java
+++ src/main/java/com/alibaba/demo/config/ErrorHandler.java
@@ -12,20 +12,20 @@ public class ErrorHandler {
     //硬编码 正常捕获
     //但无法捕获所有发送的异常
     //与LoggingHandler 平均分摊异常消息
-    @ServiceActivator(inputChannel = "MQ_INST_123123_Bcw0lhYs%test-topic.errors")
+    @ServiceActivator(inputChannel = "MQ_INST_123123_Bcw0lhYs%test-topic.test-group1.errors")
     public void error1(MessagingException message) {
         log.warn("捕获异常 Handling ERROR: " + message.getFailedMessage().getPayload());
     }
 
-    //参考官方文档 无法捕获
-    @ServiceActivator(inputChannel = CustomBinding.CUSTOMOUTPUT+".errors")
-    public void error2(MessagingException message) {
-        log.warn("捕获异常 Handling ERROR: " + message.getFailedMessage().getPayload());
-    }
+   //参考官方文档 无法捕获
+   //@ServiceActivator(inputChannel = CustomBinding.CUSTOMOUTPUT+".errors")
+   //public void error2(MessagingException message) {
+   //     log.warn("捕获异常 Handling ERROR: " + message.getFailedMessage().getPayload());
+   // }
 
     //SPEL无法捕获
-    @ServiceActivator(inputChannel ="${spring.cloud.stream.bindings."+CustomBinding.CUSTOMOUTPUT+".errors")
-    public void error3(MessagingException message) {
-        log.warn("捕获异常 Handling ERROR: " + message.getFailedMessage().getPayload());
-    }
+    //@ServiceActivator(inputChannel ="${spring.cloud.stream.bindings."+CustomBinding.CUSTOMOUTPUT+".errors")
+    //public void error3(MessagingException message) {
+    //    log.warn("捕获异常 Handling ERROR: " + message.getFailedMessage().getPayload());
+    //}
 }
